6b7ae04e3b3a9ce26a609658423b79cb310d78d9
Jon Hendrix <skybound987@protonmail.com>
2024-01-19 20:05:44 -0500

    Updated thrust and rotation physics.  Corrected laser direction by changing global_position to global_transform, laser position now rotates with the player.  Added death function.  Added health and hit_points.  Corrected collision masks and connected player_body_entered node.  Mobs trigger death function succesfully.  Reduced torque which reduced rotation speed, increased thrust value.

--- Player.gd
+++ Player.gd
@@ -10,22 +10,34 @@ signal hit
 @export var speed = 400 # Player Speed (pixels/sec)
 var score
 var screen_size
+var health = 5
 var alive = true
 var next_fire_time = 0 # controls the rate of fire
 var fire_rate = .1 # seconds between shots
 var max_speed: float = 400.0
-var thrust = Vector2(0, -250)
-var torque = 20000
+var thrust = Vector2(0, -450)
+var torque = 2500
 
 
 
-# Player collision function
 func _player_body_entered(body):  # If an object enters the players collision body
+	print("Player Hit!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+	_player_damage()
+
+func _player_dead():
 	alive = false
-	hide() # Player disappears after being hit.
-	hit.emit() # CollisionShape2D must be deferred as we can't change physics properties on a physics callback.
-	$PlayerCollision.set_deferred("disabled", true) # Turn off the collision when hit
 	$ShipExplode.play()
+	hide() # Player disappears after being hit.
+#	hit.emit() # CollisionShape2D must be deferred as we can't change physics properties on a physics callback.
+	$PlayerCollision.set_deferred("disabled", true) # Turn off the collision when hit
+
+func _player_damage(hit_points = 5):
+	$ShipDamage.play()
+	health -= hit_points
+	print("Damage Damage Damage")
+	if health <= 0:
+		_player_dead()
+
 
 # Game reset function
 func start(pos):
@@ -36,17 +48,14 @@ func start(pos):
 
 # Fire function
 func fire():
-	var laser_instance_left = laser_scene.instantiate()
+	var laser_instance_left = laser_scene.instantiate()    #  Holds laser instance for spawning
 	var laser_instance_right = laser_scene.instantiate()
-	var left_gun_position = $LeftGunPosition.global_position
-	var right_gun_position = $RightGunPosition.global_position
-
-	laser_instance_left.global_position = left_gun_position
-	laser_instance_right.global_position = right_gun_position
-
-	get_tree().root.add_child(laser_instance_left)
+	get_tree().root.add_child(laser_instance_left)         #  Add instance to the scene tree
 	get_tree().root.add_child(laser_instance_right)
-	laser_instance_left.laser_sound()  # If placed before the instance is created in the root tree, error
+	laser_instance_left.transform = $LeftGunPosition.global_transform    #  Assign transform to allow rotation
+	laser_instance_right.transform = $RightGunPosition.global_transform	
+	laser_instance_left.laser_sound() 
+	
 
 
 # Called once at start
@@ -57,12 +66,12 @@ func _ready():
 # Called every frame
 func _physics_process(delta):
 	var velocity: Vector2 = Vector2.ZERO
-
+	
 	if alive:  # Player controls below only function if player is alive
 		var moving = false	
 #		if velocity.length() > max_speed:  # Caps max speed
 #			velocity = velocity.normalized() * max_speed
-
+		
 		# Player Sounds
 		if moving and not $EngineTurn.is_playing():
 			$EngineTurn.play()
@@ -70,11 +79,11 @@ func _physics_process(delta):
 			$EngineTurn.stop()
 
 		# Animations for Player Sprites
-		if velocity.length() > 0:
-			velocity = velocity.normalized() * speed
-			$MovementSprite.play()
-		else:
-			$MovementSprite.stop()
+#		if velocity.length() > 0:
+#			velocity = velocity.normalized() * speed
+#			$MovementSprite.play()
+#		else:
+#			$MovementSprite.stop()
 
 		# Flips animation horizontal or vertically
 	#	if velocity.x != 0:
@@ -85,9 +94,9 @@ func _physics_process(delta):
 	#		animated_sprite.animation = "vertical"
 	#		animated_sprite.flip_v = velocity.y > 0
 
-		# Clamping
-		position += velocity * delta
-		position = position.clamp(Vector2.ZERO, screen_size)
+#		# Clamping
+#		position += velocity * delta
+#		position = position.clamp(Vector2.ZERO, screen_size)
 
 		# Handles Primary Weapons Fire
 #		if Input.is_action_just_pressed("primary_fire"):
@@ -101,17 +110,20 @@ func _physics_process(delta):
 	elif $EngineTurn.is_playing():
 		$EngineTurn.stop()
 
-func _integrate_forces(state):    # Handle Thrust and Rotation
+func _integrate_forces(state):    # More physics control and options compared to _physics_process()
+	
+	var rotation_direction = 0
+	var contact_count = get_contact_count()
 	
 	if Input.is_action_pressed("move_up"):    # Thrust input
 		state.apply_force(thrust.rotated(rotation))
 	else:
 		state.apply_force(Vector2())
-	
-	var rotation_direction = 0    # Rotation input
-	if Input.is_action_pressed("move_right"):
+		
+	if Input.is_action_pressed("move_right"):    # Rotation input
 		rotation_direction += 1
 	if Input.is_action_pressed("move_left"):
 		rotation_direction -= 1
 		
-	state.apply_torque(rotation_direction * torque)
+	state.apply_torque(rotation_direction * torque)  #  Enables the movement to happen
+	
